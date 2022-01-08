aaaee46af264b861f4379a52e56ca3187cf6b4ca
jose12836 <jose12836@gmail.com>
2022-01-08 00:10:10 -0700

    obtener corte terminado

--- php/request/getCorte.php
+++ php/request/getCorte.php
@@ -4,26 +4,58 @@ require("../connections/connection.php");
 $tipoCort= $_POST["option"];
 $usuario = $_SESSION['nombre'];
 if ($tipoCort == 'Turno') {
-    $consulta = "SELECT  vent.cantidad, vent.precio, art.costo_compra FROM ventas vent LEFT JOIN articulos art ON vent.articulo = art.id WHERE fecha = '".date('Y-m-d')."' AND empleado = '".$usuario."' ";
+    $consulta = "SELECT  vent.cantidad, vent.precio, art.costo_compra FROM ventas vent LEFT JOIN articulos art ON vent.articulo = art.id WHERE fecha = '".date('Y-m-d')."' AND empleado = '".$usuario."'";
     $ventas = mysqli_query($mysqli, $consulta);
-    $fondoCaja = "SELECT efectivo FROM efectivo WHERE fecha = '".date('Y-m-d')."' AND concepto = 'Fondo de Caja' AND ISNULL(corte)";
-    $fondoCajaResp = mysqli_query($mysqli, $fondoCaja)->fetch_assoc();
-    if($ventas)
-    {
+    if($ventas){
         $ventasTotatles = 0;
         $ventasSinGanancia = 0;
+        
         while($venta = mysqli_fetch_assoc($ventas)){
             $ventasTotatles += $venta['cantidad']*$venta['precio'];
             $ventasSinGanancia += $venta['cantidad']*$venta['costo_compra'];
     
         }
     }
+    $fondoCaja = "SELECT efectivo,concepto,eos FROM efectivo WHERE fecha = '".date('Y-m-d')."' AND ISNULL(corte)";
+    $fondoCajaResp = mysqli_query($mysqli, $fondoCaja);
+    if($fondoCajaResp){
+        $ventasEfectivo = 0;
+        while($efectCaja = mysqli_fetch_assoc($fondoCajaResp)){
+            if ($efectCaja['concepto'] == 'Fondo de Caja') {
+                $fondoInicial = $efectCaja['efectivo'];
+            }
+            if ($efectCaja['eos'] == 'entrada' && $efectCaja['concepto'] == 'Venta') {
+                $ventasEfectivo += $efectCaja['efectivo'];
+            }
+            if ($efectCaja['eos'] == 'entrada' && $efectCaja['concepto'] == 'Recibo de Dinero') {
+                $entradasEfectivo += $efectCaja['efectivo'];
+            }
+            if ($efectCaja['eos'] == 'salida') {
+                $salidaEfectivo += $efectCaja['efectivo'];
+            }
+        }
+    }
+    $ventaTarjeta = "SELECT efectivo,concepto,eos FROM tarjeta WHERE fecha = '".date('Y-m-d')."' AND ISNULL(corte)";
+    $ventaTarjetaResp = mysqli_query($mysqli, $ventaTarjeta);
+    if($ventaTarjetaResp){
+        $ventasTarjeta = 0;
+        while($efectTarjeta = mysqli_fetch_assoc($ventaTarjetaResp)){
+            if ($efectTarjeta['eos'] == 'entrada' && $efectTarjeta['concepto'] == 'Venta') {
+                $ventasTarjeta += $efectTarjeta['efectivo'];
+            }
+        }
+    }
+    
 }
 if($ventas){
     echo json_encode(array('status'=>true,'message'=>[
         'ventasTotatles'=>$ventasTotatles,
         'ganancias'=>$ventasTotatles-$ventasSinGanancia,
-        'fondoCaja'=>intVal($fondoCajaResp['efectivo']),
+        'fondoInicial'=>intVal($fondoInicial),
+        'ventasEfectivo'=>intVal($ventasEfectivo),
+        'entradasEfectivo'=>intVal($entradasEfectivo),
+        'salidaEfectivo'=>intVal($salidaEfectivo),
+        'ventasTarjeta'=>intVal($ventasTarjeta),
     ]));
 }else{
     echo json_encode(array('status'=>false,'message'=>mysqli_error($mysqli)));
